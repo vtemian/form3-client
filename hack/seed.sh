bad51d6d5063dd9a61bf48bb09bc0175fe2e4011
Vlad Temian <vladtemian@gmail.com>
2020-09-25 14:02:25 +0300

    Clean db before we run the tests

--- hack/seed.sh
+++ hack/seed.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-HOST=${HOST:-"localhost:8080"}
+HOST=${HOST:-"http://localhost:8080"}
+
+echo "Deleting all accounts"
+for id in $(curl -s "${HOST}/v1/organisation/accounts" | jq ".data[] | .id"); do
+  curl -s -X DELETE "${HOST}/v1/organisation/accounts/${id}?version=0" > /dev/null || true;
+done;
 
 # Importing only account fixtures
 for fixture in ./fixtures/fetch_*.json; do
   echo "Importing $fixture"
 
-  curl -s -X POST "http://${HOST}/v1/organisation/accounts" \
+  curl -s -X POST "${HOST}/v1/organisation/accounts" \
      --header "Content-Type: application/json" \
      --data-raw "$(cat $fixture)" > /dev/null || true;
 done;
