4ce752a49955f28b66d99ced2a4f18904d75c1c5
Vlad Temian <vladtemian@gmail.com>
2020-09-29 15:00:48 +0300

    Switch too host:port scheme

--- docker/api/test_entrypoint.sh
+++ docker/api/test_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/dumb-init /bin/sh
 set -e
 
-dockerize -wait "${VAULT_ADDR}" -wait "tcp://${PSQL_HOST}:${SQL_PORT}"
+dockerize -wait "tcp://${VAULT_HOST}:${VAULT_PORT}" -wait "tcp://${PSQL_HOST}:${SQL_PORT}"
 
 exec /app/entrypoint.sh
