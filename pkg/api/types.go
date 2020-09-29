cd3a04c95b161100462620d3d3e04bf4d0d18c04
Vlad Temian <vladtemian@gmail.com>
2020-09-29 18:00:56 +0300

    Use AccountClassification to specify account type

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -12,20 +12,27 @@ type OrganisationResource struct {
 	OrganisationID string `json:"organisation_id"`
 }
 
+type AccountClassification string
+
+const (
+	AccountClassificationPersonal AccountClassification = "Personal"
+	AccountClassificationBusiness AccountClassification = "Business"
+)
+
 type AccountAttributes struct {
-	Country                 string   `json:"country"`
-	BaseCurrency            string   `json:"base_currency"`
-	AccountNumber           string   `json:"account_number"`
-	BankID                  string   `json:"bank_id"`
-	BankIDCode              string   `json:"bank_id_code"`
-	BIC                     string   `json:"bic"`
-	IBAN                    string   `json:"iban"`
-	Name                    []string `json:"name"`
-	AlternativeNames        []string `json:"alternative_names"`
-	AccountClassification   string   `json:"account_classification"`
-	SecondaryIdentification string   `json:"secondary_identification"`
-	Switched                bool     `json:"switched"`
-	Status                  string   `json:"status"`
+	Country                 string                `json:"country"`
+	BaseCurrency            string                `json:"base_currency"`
+	AccountNumber           string                `json:"account_number"`
+	BankID                  string                `json:"bank_id"`
+	BankIDCode              string                `json:"bank_id_code"`
+	BIC                     string                `json:"bic"`
+	IBAN                    string                `json:"iban"`
+	Name                    []string              `json:"name"`
+	AlternativeNames        []string              `json:"alternative_names"`
+	AccountClassification   AccountClassification `json:"account_classification"`
+	SecondaryIdentification string                `json:"secondary_identification"`
+	Switched                bool                  `json:"switched"`
+	Status                  string                `json:"status"`
 }
 
 type Account struct {
