d237b62b4e1405a5c2ca95f299014b6da5ce1ced
Vlad Temian <vladtemian@gmail.com>
2020-09-29 15:25:26 +0300

    Put back silences

--- hack/seed.sh
+++ hack/seed.sh
@@ -3,15 +3,15 @@
 HOST=${TEST_API_HOST:-"http://localhost:8080"}
 
 echo "Deleting all accounts"
-for id in $(curl "${HOST}/v1/organisation/accounts" | jq ".data[] | .id"); do
-  curl -X DELETE "${HOST}/v1/organisation/accounts/${id}?version=0" || true;
+for id in $(curl -s "${HOST}/v1/organisation/accounts" | jq ".data[] | .id"); do
+  curl -s -X DELETE "${HOST}/v1/organisation/accounts/${id}?version=0" > /dev/null || true;
 done;
 
 # Importing only account fixtures
 for fixture in ./fixtures/fetch_*.json; do
   echo "Importing $fixture"
 
-  curl -X POST "${HOST}/v1/organisation/accounts" \
+  curl -s -X POST "${HOST}/v1/organisation/accounts" \
      --header "Content-Type: application/json" \
-     --data-raw "$(cat $fixture)" || true;
+     --data-raw "$(cat $fixture)" > /dev/null || true;
 done;
