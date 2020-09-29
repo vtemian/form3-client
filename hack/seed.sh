3f1fd263807748d53319503cbf0111045a076e67
Vlad Temian <vladtemian@gmail.com>
2020-09-29 13:31:47 +0300

    Debug seed script

--- hack/seed.sh
+++ hack/seed.sh
@@ -3,15 +3,15 @@
 HOST=${TEST_API_HOST:-"http://localhost:8080"}
 
 echo "Deleting all accounts"
-for id in $(curl -s "${HOST}/v1/organisation/accounts" | jq ".data[] | .id"); do
-  curl -s -X DELETE "${HOST}/v1/organisation/accounts/${id}?version=0" > /dev/null || true;
+for id in $(curl "${HOST}/v1/organisation/accounts" | jq ".data[] | .id"); do
+  curl -X DELETE "${HOST}/v1/organisation/accounts/${id}?version=0" || true;
 done;
 
 # Importing only account fixtures
 for fixture in ./fixtures/fetch_*.json; do
   echo "Importing $fixture"
 
-  curl -s -X POST "${HOST}/v1/organisation/accounts" \
+  curl -X POST "${HOST}/v1/organisation/accounts" \
      --header "Content-Type: application/json" \
      --data-raw "$(cat $fixture)" || true;
 done;
